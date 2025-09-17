b4285c110b26c98d694dbcca666912949dbf0f4f
delilaisrn <delilaisrn@gmail.com>
2025-09-17 11:03:46 +0700

    tugas 3

--- main/views.py
+++ main/views.py
@@ -1,10 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.core import serializers
+from main.models import Product
+from main.forms import ProductForm
 
 def show_main(request):
+    products = Product.objects.all()
     context = {
         'app_name': 'Saturn Sports Station (SSS)',
         'name': 'Delila Isrina Aroyo',
-        'class': 'PBP A'
+        'class': 'PBP A',
+        'product_list': products,
     }
-
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('main:show_main')
+    return render(request, "create_product.html", {'form': form})
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, "product_detail.html", {'product': product})
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    try:
+        data = Product.objects.filter(pk=id)
+        return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    except Product.DoesNotExist:
+       return HttpResponse(status=404)
+
+def show_json_by_id(request, id):
+    try:
+        data = Product.objects.filter(pk=id)
+        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    except Product.DoesNotExist:
+       return HttpResponse(status=404)
