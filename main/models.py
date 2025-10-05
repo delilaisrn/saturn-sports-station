263625d9e93c8d7154b270664fdc4552cfb160bf
delilaisrn <delilaisrn@gmail.com>
2025-10-05 13:22:46 +0700

    memperbaiki tampilan web

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     thumbnail = models.URLField()
     category = models.CharField(max_length=50)
